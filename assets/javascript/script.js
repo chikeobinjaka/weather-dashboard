85c5a5a02f07d94d7b991a1c2ef2fe981dcf73e4
Chikeobi <chikeobi.njaka@gmail.com>
2020-04-05 22:30:18 -0700

    HTML Page mockup completed

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+const DEGREE_FAHRENHEIT = "&#8457;";
+const DEGREE_CELCIUS = "&#8451;";
+const DEFAULT_CITY = "San Clemente, California";
+const OPEN_WEATHER_APPID = "909d9a0c309725bb1dee62d3d268a4ee";
+var SEARCH_CITY = "San+Clemente";
+var OPEN_WEATHER_FIVE_DAY_FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${SEARCH_CITY}&appid=${OPEN_WEATHER_APPID}`;
+var dt = new Date("2020-04-06 12:00:00");
